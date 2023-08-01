8315d94b9b627de392835c8f58c064a249cb0875
m4zeboy <moises775azevedo@gmail.com>
2023-08-01 16:28:11 -0300

    List tasks route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import { createTask } from './controllers/create-task.js';
+import { listTasks } from './controllers/list-tasks.js'
 import { Router } from './lib/router.js';
 
 export const router = new Router();
 
 router.post("/tasks", createTask);
+router.get("/tasks", listTasks);
