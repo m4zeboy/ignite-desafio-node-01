cbf69bc0ea2a8b2e917e90ca6e4755346606f08c
m4zeboy <moises775azevedo@gmail.com>
2023-08-01 11:51:10 -0300

    Create task route

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
+import { createTask } from './controllers/create-task.js';
 import { Router } from './lib/router.js';
 
 export const router = new Router();
 
-router.post("/tasks", (req, res) => {
-  return res.end();
-})
+router.post("/tasks", createTask);
