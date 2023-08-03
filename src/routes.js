8c01c45e629df61f84af045410bb2be37ce9b4f8
m4zeboy <moises775azevedo@gmail.com>
2023-08-03 14:54:09 -0300

    Update task route

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { createTask } from './controllers/create-task.js';
 import { listTasks } from './controllers/list-tasks.js'
+import { updateTask } from './controllers/update-task.js';
 import { Router } from './lib/router.js';
 
 export const router = new Router();
 
 router.post("/tasks", createTask);
 router.get("/tasks", listTasks);
+router.put("/tasks/:id", updateTask)
