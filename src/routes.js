fc0eba9a559bf9c04583d203f7a36b7cf28b604f
m4zeboy <moises775azevedo@gmail.com>
2023-08-03 15:03:38 -0300

    Delete task route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
+import { Router } from './lib/router.js';
 import { createTask } from './controllers/create-task.js';
 import { listTasks } from './controllers/list-tasks.js'
 import { updateTask } from './controllers/update-task.js';
-import { Router } from './lib/router.js';
+import { deleteTask } from './controllers/delete-task.js';
 
 export const router = new Router();
 
 router.post("/tasks", createTask);
 router.get("/tasks", listTasks);
 router.put("/tasks/:id", updateTask)
+router.delete("/tasks/:id", deleteTask);
