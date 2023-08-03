43251c29e0038729aad726a47f119bfbc482fe63
m4zeboy <moises775azevedo@gmail.com>
2023-08-03 14:53:31 -0300

    Fix regex

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,5 +1,5 @@
 export function buildRoutePath(path) {
-  const routeParametersRegex = /:([a-zA-z] + )/g;
+  const routeParametersRegex = /:([a-zA-z]+)/g;
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)');
   const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`);
 
