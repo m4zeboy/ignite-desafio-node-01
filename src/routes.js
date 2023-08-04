d72e8d5853c81ab2c9bfdfb85966410b5573be09
m4zeboy <moises775azevedo@gmail.com>
2023-08-04 15:36:35 -0300

    Complete task route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { createTask } from './controllers/create-task.js';
 import { listTasks } from './controllers/list-tasks.js'
 import { updateTask } from './controllers/update-task.js';
 import { deleteTask } from './controllers/delete-task.js';
+import { completeTask } from './controllers/complete-task.js';
 
 export const router = new Router();
 
@@ -10,3 +11,4 @@ router.post("/tasks", createTask);
 router.get("/tasks", listTasks);
 router.put("/tasks/:id", updateTask)
 router.delete("/tasks/:id", deleteTask);
+router.patch("/tasks/:id/complete", completeTask);
